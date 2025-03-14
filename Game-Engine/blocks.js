9e047ca1e51b6d304259b17cba0d90bba00b54c0
alessioiskuhl <109654241+alessioiskuhl@users.noreply.github.com>
2025-03-14 16:07:20 +0100

    Update blocks.js
    Added blockly

--- Game-Engine/blocks.js
+++ Game-Engine/blocks.js
@@ -1,1 +1,23 @@
+import * as Blockly from "blockly";
+
+// Define a custom move block
+Blockly.Blocks['move'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Move")
+            .appendField(new Blockly.FieldNumber(10), "STEPS")
+            .appendField("steps");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+// Generate JavaScript code from blocks
+Blockly.JavaScript['move'] = function(block) {
+    var steps = block.getFieldValue('STEPS');
+    return `move(${steps});\n`;
+};
 
