bd1fa29cc69c2a2a2a137b5b6c23a8d4878e5c5d
alessioiskuhl <109654241+alessioiskuhl@users.noreply.github.com>
2025-03-14 21:06:26 +0100

    Create app.py
    Created the app.py for flask

---
+++ Game-Engine/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/run')
+def run_python_script():
+    with open("script.py", "r") as file:
+        code = file.read()
+    return f"<pre>{code}</pre>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='8.8.0.0', port=3000)
