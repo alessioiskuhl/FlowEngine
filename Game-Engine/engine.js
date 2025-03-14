06a06c0750fee53ef4fb29ff5cb8885ce394d41c
alessioiskuhl <109654241+alessioiskuhl@users.noreply.github.com>
2025-03-14 16:10:14 +0100

    Update engine.js
    Created an execution engine

--- Game-Engine/engine.js
+++ Game-Engine/engine.js
@@ -1,1 +1,12 @@
+let sprite = { x: 100, y: 100 };
+
+function move(steps) {
+    sprite.x += steps;
+}
+
+// Run blocks when button is clicked
+document.getElementById("run").addEventListener("click", function() {
+    const code = Blockly.JavaScript.workspaceToCode(workspace);
+    eval(code);
+});
 
